57b3210d5d9102a426eb19cc2af75c11834e5f16
vamshisuram <vamshi.suram25@gmail.com>
2014-06-16 13:21:40 +0530

    schema defination moved to model's file; Fixes #4

--- app/models/book.js
+++ app/models/book.js
@@ -1,3 +1,19 @@
+var mongoOpt = { url: 'mongodb://localhost/nineleaps_library' };
+
+var Schema = require('jugglingdb').Schema;
+var mongoSchema = new Schema('mongodb', mongoOpt);
+
 module.exports = function (compound, Book) {
-  // define Book here
+  var Book = mongoSchema.define('Book', {
+    id: {type: Number},
+    title: {type: String},
+    authors: {type: String, default: new Array()},
+    category: {type: String},
+    level: {type: String},
+    availability: {type: String},
+    numOfCopies: {type: Number},
+    summary: {type: String}
+  });
+
+  compound.models.Book = Book;
 };
